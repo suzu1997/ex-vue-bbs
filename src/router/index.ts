d66a63b91474f20044ff19ff470ccb51758568ca
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-12 12:24:15 +0900

    全てのパスで掲示板コンポーネントへ遷移するようにルーティングを設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,13 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
+  // 全てのパスを掲示板コンポーネントへ遷移する
   {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "*",
+    component: () => import("../components/Bbs.vue"),
   },
 ];
 
