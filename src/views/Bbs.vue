c22bd004a462c5704b0d35629925c9e2856da0d3
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-12 13:43:48 +0900

    掲示板画面の表示機能を表すクラスコンポーネントの記事一覧表示に拘る部分を作成

--- src/views/Bbs.vue
+++ src/views/Bbs.vue
@@ -1,5 +1,25 @@
 <template>
   <div id="app">掲示板画面</div>
 </template>
-<script lang="“ts”"></script>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+/**
+ * 掲示板画面の表示機能を表すクラスコンポーネント.
+ */
+@Component
+export default class Bbs extends Vue {
+  // 現在の記事一覧
+  private currentArticleList = [];
+
+  /**
+   * 記事一覧を表示する.
+   *
+   * @remarks Vuexストア内の記事一覧(articles)をcurrentArticleListに格納する
+   */
+  created(): void {
+    this.currentArticleList = this.$store.getters.getArticles;
+  }
+}
+</script>
 <style scoped></style>
