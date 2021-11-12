24b28cd2bf5e598eb7f71fed810e8e624481bd94
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-12 13:42:56 +0900

    Vuexストアに記事一覧情報とそれを返すgetterを追加

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,36 @@
+import { Article } from "@/types/article";
+import { Comment } from "@/types/comment";
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  strict: true,
+  state: {
+    // 記事一覧の情報
+    articles: [
+      new Article(3, "佐藤", "佐藤さんの記事", []),
+      new Article(2, "山田", "山田さんの記事", [
+        new Comment(13, "山崎", "山崎さんのコメント", 2),
+      ]),
+      new Article(1, "伊藤", "伊藤さんの記事", [
+        new Comment(12, "鈴木", "鈴木さんのコメント", 1),
+        new Comment(11, "吉田", "吉田さんのコメント", 1),
+      ]),
+    ],
+  }, // end state
+
   mutations: {},
-  actions: {},
-  modules: {},
+  getters: {
+    /**
+     * 記事一覧を返す.
+     *
+     * @param state - Vuexのstateオブジェクト
+     * @returns 記事一覧の情報
+     */
+    getArticles(state) {
+      return state.articles;
+    },
+  },
 });
